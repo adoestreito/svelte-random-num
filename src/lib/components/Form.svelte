3374c0044d77565e11e21315328683765e2f593f
adoestreito <adestreito@fastly.com>
2025-02-05 12:04:49 +0100

    Adding tailwind css

--- src/lib/components/Form.svelte
+++ src/lib/components/Form.svelte
@@ -1,22 +1,59 @@
 <script>
 	let min = $state(0);
 	let max = $state(10);
-	let random = $derived(Math.floor(Math.random() * max));
+	let placeholder = $derived(Math.floor(Math.random() * (max - min + 1)) + min);
 
 	function oninputmin(event) {
-		console.log(event.data);
-		min = event.data;
+		min = +event.target.value;
+		console.log(min);
 	}
 	function oninputmax(event) {
-		console.log(event.data);
-		max = event.data;
+		max = +event.target.value;
+		console.log(max);
+	}
+
+	function onclick() {
+		console.log(`Min: ${min}, Max: ${max}`);
+		// placeholder = Math.floor(Math.random() * (max - min + 1)) + min;
+		console.log(placeholder);
 	}
 </script>
 
-<label for="min">Min</label>
-<input type="number" name="min" id="min" oninput={oninputmin} />
-<label for="max">Max</label>
-<input type="number" name="max" id="max" oninput={oninputmax} />
-<h1>{random}</h1>
+<p>Generate a random number</p>
 
-<style></style>
+<form class="w-full max-w-lg">
+	<div class="-mx-3 mb-6 flex flex-wrap">
+		<div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+			<label class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700" for="min">
+				Min. Value
+			</label>
+			<input
+				class="mb-3 block w-full appearance-none rounded border border-red-500 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+				id="min"
+				type="number"
+				placeholder="Min value"
+				onchange={oninputmin}
+			/>
+		</div>
+		<div class="w-full px-3 md:w-1/2">
+			<label
+				class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+				for="grid-last-name"
+			>
+				Max. value
+			</label>
+			<input
+				class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+				id="grid-last-name"
+				type="number"
+				placeholder="Max. Value"
+				onchange={oninputmax}
+			/>
+		</div>
+	</div>
+</form>
+
+<h1 class="">The random number is: {placeholder}</h1>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,4 @@
 	import Form from '$lib/components/Form.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Form />
