40ee1a52b83821edf25c90d3393d35049573c0f6
adoestreito <adestreito@fastly.com>
2025-02-07 10:08:31 +0100

    Adding vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 	let { children } = $props();
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectAnalytics();
+	injectSpeedInsights();
 </script>
 
 {@render children()}
